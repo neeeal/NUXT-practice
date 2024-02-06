de44820979464274aa40e3d8d60546fc35132b45
Niil <91337648+neeeal@users.noreply.github.com>
2024-02-06 11:32:57 +0800

    studied composables.

--- pages/custom.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-    definePageMeta({
-        layout: "custom"
-    });
-</script>
-
-<template>
-    <div class="Custom page">Custom page</div>
-</template>
--- pages/profile.vue
+++ pages/profile.vue
@@ -1,10 +1,9 @@
 <script setup>
-const { sayHello } = useUtils()
-sayHello()
+
 </script>
 
 <template>
     <div class="container py-3 mx-auto Main">
-        <h1 class="text-teal-500">Hello Nuxt 3</h1>
+        <h1 class="text-teal-500">Profile</h1>
     </div>
 </template>
