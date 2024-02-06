dab42afcc14cabae02519d449a90921fe14b0d20
Niil <91337648+neeeal@users.noreply.github.com>
2024-02-06 11:07:11 +0800

    studied assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   alias: {
-    '@':resolve(__dirname, "/")
+    assets: "/<rootDir>/assets"
   },
   css: [
     "~/assets/main.scss"
